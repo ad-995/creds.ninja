04aff7982e5bc0d29508260c30674e8abe56e44a
Michael Ranaldo <michael@ranaldo.co.uk>
2019-12-19 00:40:43 +0000

    Added search function and table creation while watching films too late

--- app/js/creds.ninja.js
+++ app/js/creds.ninja.js
@@ -1,35 +1,58 @@
-function undateables(json) {
-	console.log(json)
+function create_table(json, searchterm) {
+	var results_count = 0;
 	var table = document.createElement("table");
+	table.setAttribute("class","centered highlight");
+	var thead = document.createElement("thead");
 	var tbody = document.createElement("tbody");
-	var target = $('#results')
-	if (Object.keys(json).length > 0) {
-		var table_headers = ['vendor', 'username', 'password']
-		var headers = document.createElement("tr");
-		for (const header_index in table_headers) {
-			var cell = document.createElement("th");
-			var text = document.createTextNode(table_headers[header_index]);
-			cell.appendChild(text)
-			headers.appendChild(cell)
-		}
-		tbody.appendChild(headers)
-		for (const [key, value] of Object.entries(json)) {
-			var row = document.createElement("tr");
-			for (var i = 0; i < 3; i++) {
-				var cell = document.createElement("td");
-				var text = document.createTextNode(value[table_headers[i]])
-				cell.appendChild(text)
-				row.appendChild(cell)
+	var table_headers = ['vendor', 'username', 'password']
+	var headers = document.createElement("tr");
+
+	for (const header_index in table_headers) {
+		var cell = document.createElement("th");
+		var text = document.createTextNode(table_headers[header_index]);
+		cell.appendChild(text)
+		headers.appendChild(cell)
+	}
+
+	thead.appendChild(headers)
+	table.appendChild(thead)
+
+	for (const [key, value] of Object.entries(json)) {
+		while (results_count < 50) {
+			if (value["vendor"].includes(searchterm)) {
+				var row = document.createElement("tr");
+				for (var i = 0; i < 3; i++) {
+					var cell = document.createElement("td");
+					var text = document.createTextNode(value[table_headers[i]])
+					cell.appendChild(text)
+					row.appendChild(cell)
+				}
+				tbody.appendChild(row)
+				results_count++;
 			}
-			tbody.appendChild(row)
 		}
+	}
+
+	if (results_count < 50) {
 		table.appendChild(tbody)
-		$("#results").append(table)
+		console.log(results_count)
+		return table
 	} else {
 		var header = document.createElement("h2");
 		var header_text = document.createTextNode(Object.keys(json).length + " results found! Try narrowing it down")
 		header.appendChild(header_text)
+		return header
 	}
+}
+
+function undateables(json) {
+	var target = $('#results')
+	// for all credentials
+	// if we have more than none returned
+	var search_term = document.getElementById("search").value
+
+	var results = create_table(json, search_term)
+	$("#results").append(results)
 }
 
 fetch('/creds.json')
