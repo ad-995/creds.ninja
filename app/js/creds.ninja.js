03acdc25bf4529327645acd209b646fc1bd52a82
mez0cc <mez0@pm.me>
2019-12-20 10:31:04 +0000

    added export to file with json

--- app/js/creds.ninja.js
+++ app/js/creds.ninja.js
@@ -22,13 +22,56 @@ function html_to_json(table) {
         data.push(rowData);
     }       
 
-    return data;
+    var json = JSON.stringify(data);
+    return json;
+}
+
+function download(strData, strFileName, strMimeType) {
+	// https://stackoverflow.com/questions/21012580/is-it-possible-to-write-data-to-file-using-only-javascript
+    var D = document,
+        A = arguments,
+        a = D.createElement("a"),
+        d = A[0],
+        n = A[1],
+        t = A[2] || "text/plain";
+
+    //build download link:
+    a.href = "data:" + strMimeType + "charset=utf-8," + escape(strData);
+
+    if (window.MSBlobBuilder) { // IE10
+        var bb = new MSBlobBuilder();
+        bb.append(strData);
+        return navigator.msSaveBlob(bb, strFileName);
+    } /* end if(window.MSBlobBuilder) */
+
+    if ('download' in a) { //FF20, CH19
+        a.setAttribute("download", n);
+        a.innerHTML = "downloading...";
+        D.body.appendChild(a);
+        setTimeout(function() {
+            var e = D.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(e);
+            D.body.removeChild(a);
+        }, 66);
+        return true;
+    }; /* end if('download' in a) */
+
+    //do iframe dataURL download: (older W3)
+    var f = D.createElement("iframe");
+    D.body.appendChild(f);
+    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+    setTimeout(function() {
+        D.body.removeChild(f);
+    }, 333);
+    return true;
 }
 
 function export_table(){
     const table_html = document.getElementById("results_table");
+    const export_button = document.getElementById("export");
     table_json = html_to_json(table_html);
-    console.log(table_json);
+    download(table_json, 'creds.ninja.json', 'application/json');
 }
 
 function refresh_div() {
